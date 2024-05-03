4c92d5450bc703f532c4a1bfe6be9af76981c72a
Belen Mar <20690449@tecvalles.mx>
2024-05-03 15:43:02 -0600

    contador de las cartas

--- lib/main.dart
+++ lib/main.dart
@@ -5,52 +5,84 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<Widget> jugador = List.generate(4, (index) => RandomColorCard());
-    List<Widget> computadora = List.generate(4, (index) => RandomColorCard());
-
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: GridView.count(
-                    crossAxisCount: 4,
-                    children: jugador,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20), 
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: GridView.count(
-                    crossAxisCount: 4,
-                    children: computadora,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: Scoreboard(),
     );
   }
 }
 
+class Scoreboard extends StatefulWidget {
+  @override
+  _ScoreboardState createState() => _ScoreboardState();
+}
+
+class _ScoreboardState extends State<Scoreboard> {
+  int puntuacion = 0; // Puntuación total para ambos jugadores
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> jugador = List.generate(4, (index) => RandomColorCard(actualizarPuntuacion));
+    List<Widget> computadora = List.generate(4, (index) => RandomColorCard(actualizarPuntuacion));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MINI ONO'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Marcador',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '$puntuacion',
+              style: TextStyle(fontSize: 36),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: GridView.count(
+                  crossAxisCount: 4,
+                  children: jugador,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: GridView.count(
+                  crossAxisCount: 4,
+                  children: computadora,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void actualizarPuntuacion(int numero) {
+    setState(() {
+      puntuacion += numero; // Incrementar la puntuación con el número generado aleatoriamente
+    });
+  }
+}
+
 class RandomColorCard extends StatefulWidget {
+  final Function(int) onTap;
+
+  RandomColorCard(this.onTap);
+
   @override
   _RandomColorCardState createState() => _RandomColorCardState();
 }
@@ -67,7 +99,7 @@ class _RandomColorCardState extends State<RandomColorCard> {
 
   void _generateRandom() {
     setState(() {
-      _randomNumber = Random().nextInt(9) + 1; 
+      _randomNumber = Random().nextInt(9) + 1;
       _randomColor = colors[Random().nextInt(colors.length)];
     });
   }
@@ -81,53 +113,58 @@ class _RandomColorCardState extends State<RandomColorCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15), 
-      decoration: BoxDecoration(
-        color: Colors.white, 
-        borderRadius: BorderRadius.circular(20), 
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(0, 3), 
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "$_randomNumber",
-                style: TextStyle(
-                  fontSize: 48,
-                  color: _randomColor,
-                  fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        widget.onTap(_randomNumber);
+        _generateRandom();
+      },
+      child: Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$_randomNumber",
+              style: TextStyle(
+                fontSize: 48,
+                color: _randomColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                widget.onTap(_randomNumber);
+                _generateRandom();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.cyan,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              SizedBox(height: 20), 
-              ElevatedButton(
-                onPressed: _generateRandom,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.cyan, 
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), 
-                  ),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10), 
-                  child: Text(
-                    "Poner carta",
-                    style: TextStyle(fontSize: 16),
-                  ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Text(
+                  "Poner carta",
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
