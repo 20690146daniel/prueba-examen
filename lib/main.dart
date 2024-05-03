2b678fb56f3da1536f4de5602e54bb021234aecb
Belen Mar <20690449@tecvalles.mx>
2024-05-03 14:31:37 -0600

    generar numaleatorio,color, cartas
    jugador y computador

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,134 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    List<Widget> jugador = List.generate(4, (index) => RandomColorCard());
+    List<Widget> computadora = List.generate(4, (index) => RandomColorCard());
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: GridView.count(
+                    crossAxisCount: 4,
+                    children: jugador,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20), 
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: GridView.count(
+                    crossAxisCount: 4,
+                    children: computadora,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
+class RandomColorCard extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _RandomColorCardState createState() => _RandomColorCardState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  
-  
+class _RandomColorCardState extends State<RandomColorCard> {
+  int _randomNumber = 0;
+  Color _randomColor = Colors.black;
+
+  @override
+  void initState() {
+    super.initState();
+    _generateRandom();
+  }
+
+  void _generateRandom() {
+    setState(() {
+      _randomNumber = Random().nextInt(9) + 1; 
+      _randomColor = colors[Random().nextInt(colors.length)];
+    });
+  }
+
+  final List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+  ];
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
+    return Container(
+      margin: EdgeInsets.all(15), 
+      decoration: BoxDecoration(
+        color: Colors.white, 
+        borderRadius: BorderRadius.circular(20), 
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), 
+          ),
+        ],
       ),
-      body: Center(
-     
-        child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            
-          ],
-        ),
+      child: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$_randomNumber",
+                style: TextStyle(
+                  fontSize: 48,
+                  color: _randomColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20), 
+              ElevatedButton(
+                onPressed: _generateRandom,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.cyan, 
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), 
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10), 
+                  child: Text(
+                    "Poner carta",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-    
     );
   }
 }
